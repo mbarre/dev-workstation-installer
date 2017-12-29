faaf6e3cd3206815bfd86ee86c59395153ad4d81
mbarre <michele.barre@ville-noumea.nc>
2017-12-29 15:38:15 +1100

    try to use sed to edit file

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from __future__ import with_statement
 
@@ -12,9 +12,11 @@ from fabric.contrib import files
 with open("resources/install_conf.yaml", 'r') as ymlfile:
     settings = yaml.load(ymlfile)
 
+
 @task
 def set_host():
     env.hosts = settings['hosts']
+    # env.user = settings['hosts']
 
 
 @task
@@ -23,6 +25,7 @@ def mkdir_working_directory():
 
     if not files.exists(settings['working_directory']):
         run("mkdir %s" % settings['working_directory'])
+
 
 @task
 def rm_working_directory():
@@ -41,12 +44,13 @@ def maven_install():
         mvn_url = settings['maven']['url'] % (settings['maven']['version'], mvn_artefact)
         run("wget %s" % mvn_url)
         with cd("/opt"):
-            sudo("tar xvzf %s/%s" % (settings['working_directory'],mvn_artefact))
+            sudo("tar xvzf %s/%s" % (settings['working_directory'], mvn_artefact))
             sudo("unlink maven")
             sudo("ln -s %s/apache-maven-%s maven" % ("/opt", settings['maven']['version']))
             run("export PATH=%s/%s/bin:$PATH" % ("/opt", 'maven'))
         run("rm -rf %s " % mvn_artefact)
         run("mvn -v")
+
 
 @task
 def java_install():
@@ -54,25 +58,27 @@ def java_install():
     run("apt-get install openjdk-%s-jdk" % settings['java']['version'], use_sudo=True)
     run("java -version")
 
+
 @task
 def liquibase_install():
-
     installed_liquibase = cmd('which liquibase')
     if installed_liquibase:
-        install_liquibase=True
+        install_liquibase = True
         print "Installing Liquibase %s" % settings['liquibase']['version']
     else:
         current_liquibase_version = cmd('liquibase --version')
     if current_liquibase_version != settings['liquibase']['version']:
-        install_liquibase=True
+        install_liquibase = True
         print "Updating Liquibase from %s to %s" % (current_liquibase_version, settings['liquibase']['version'])
 
     mkdir_working_directory()
     with cd(settings['working_directory']):
         if install_liquibase:
-            liquibase_url = settings['liquibase']['url'] % (settings['liquibase']['version'], settings['liquibase']['version'])
-            postgres_driver_url = settings['postgres']['driver.url'] % (settings['postgres']['driver.version'], settings['postgres']['driver.version'])
-            run("wget %s" %liquibase_url)
+            liquibase_url = settings['liquibase']['url'] % (
+                settings['liquibase']['version'], settings['liquibase']['version'])
+            postgres_driver_url = settings['postgres']['driver.url'] % (
+                settings['postgres']['driver.version'], settings['postgres']['driver.version'])
+            run("wget %s" % liquibase_url)
             sudo("dpkg -i liquibase-debian_%s_all.deb" % settings['liquibase']['version'])
             if files.exists("/opt/liquibase"):
                 sudo("unlink /opt/liquibase")
@@ -86,12 +92,14 @@ def liquibase_install():
         else:
             print "Liquibase already installed with expected version."
 
+
 @task
 def schemacrawler_install():
     mkdir_working_directory()
 
     with cd(settings['working_directory']):
-        schemacrawler_url = settings['schemacrawler']['url'] % (settings['schemacrawler']['version'], settings['schemacrawler']['version'])
+        schemacrawler_url = settings['schemacrawler']['url'] % (
+            settings['schemacrawler']['version'], settings['schemacrawler']['version'])
         run("wget %s" % schemacrawler_url)
         sudo("dpkg -i schemacrawler-deb_%s_all.deb" % settings['schemacrawler']['version'])
         sudo("mv /opt/schemacrawler/additional-lints/schemacrawler-additional-lints-*.jar /opt/schemacrawler/lib")
@@ -99,9 +107,11 @@ def schemacrawler_install():
         run("schemacrawler --version")
         run("rm -rf schemacrawler-deb_%s_all.deb" % settings['schemacrawler']['version'])
 
+
 @task
 def git_install():
     sudo("apt-get -y install git git-extras")
+
 
 @task
 def tools_install():
@@ -118,11 +128,11 @@ def tools_install():
     sudo("apt-get -y install gimp")
     sudo("apt-get -y install vim")
 
+
 @task
 def bfg_repo_cleaner_install():
     mkdir_working_directory()
     with cd(settings['working_directory']):
-
         if files.exists("bfg-repo-cleaner"):
             run("rm -rf bfg-repo-cleaner")
 
@@ -132,6 +142,7 @@ def bfg_repo_cleaner_install():
             bfg_url = settings['bfg_cleaner']['url'] % (version, version)
             run("wget %s -O bfg.jar" % bfg_url)
             sudo("chmod +x bfg.jar")
+
 
 @task
 def fakeSMTP_install():
@@ -167,15 +178,15 @@ def oh_my_zsh_install():
     with cd("~"):
         run("wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -")
 
+
 @task
 def apache_directory_studio_install():
     mkdir_working_directory()
 
     with cd(settings['working_directory']):
-
         version = settings['ads']['version']
         ads_artefact = settings['ads']['artefact'] % version
-        ads_url = settings['ads']['url']  %(version, ads_artefact)
+        ads_url = settings['ads']['url'] % (version, ads_artefact)
 
         run("wget %s" % ads_url)
         if files.exists("ApacheDirectoryStudio"):
@@ -183,6 +194,7 @@ def apache_directory_studio_install():
         run("tar xvzf %s" % ads_artefact)
 
         run("rm -rf %s" % ads_artefact)
+
 
 @task
 def apache_tomcat_install():
@@ -203,110 +215,127 @@ def apache_tomcat_install():
         run("tar xvzf %s" % artefact)
         run("ln -s apache-tomcat-%s tomcat" % version)
 
-        postgres_driver_url = settings['driver']['postgres.url'] % (settings['driver']['postgres.version'], settings['driver']['postgres.version'])
-        h2_driver_url = settings['driver']['h2.url'] % (settings['driver']['h2.version'], settings['driver']['h2.version'])
-        javax_mail_url = settings['javax.mail']['url'] % (settings['javax.mail']['version'], settings['javax.mail']['version'])
-        jt400_url = settings['driver']['jt400.url'] % (settings['driver']['jt400.version'], settings['driver']['jt400.version'])
-        activation_url = settings['activation']['url'] % (settings['activation']['version'], settings['activation']['version'])
+        postgres_driver_url = settings['driver']['postgres.url'] % (
+            settings['driver']['postgres.version'], settings['driver']['postgres.version'])
+        h2_driver_url = settings['driver']['h2.url'] % (
+            settings['driver']['h2.version'], settings['driver']['h2.version'])
+        javax_mail_url = settings['javax.mail']['url'] % (
+            settings['javax.mail']['version'], settings['javax.mail']['version'])
+        jt400_url = settings['driver']['jt400.url'] % (
+            settings['driver']['jt400.version'], settings['driver']['jt400.version'])
+        activation_url = settings['activation']['url'] % (
+            settings['activation']['version'], settings['activation']['version'])
 
-        with cd(settings['./tomcat/lib']):
+        with cd("./tomcat/lib"):
             run("wget %s" % postgres_driver_url)
             run("wget %s" % h2_driver_url)
             run("wget %s" % javax_mail_url)
             run("wget %s" % jt400_url)
             run("wget %s" % activation_url)
 
+    run("rm -rf %s" % artefact)
+
+
+def append_to_file(text, file):
+    cmd = "sed -i -e \"$a\ " + text + "\" " + file
+    print cmd
+    run(cmd)
+
 
 @task
-def edit_oh_my_zsh_rc():
+def edit_oh_my_zshrc():
     with cd("$HOME"):
-        f = open(".zshrc","r+")
-        old_lines = f.readlines()
-        f.seek(0)
+        zshrc_file = ".zshrc"
+        run("pwd")
+        run("sed -i '/plugins=(git)/d' .zshrc")
 
-        for line in old_lines:
-            if line == "plugins=(git)":
-                f.write("plugins=(%s)" % settings['oh-my-zsh']['plugins'])
-            else:
-                f.write(line)
+        # Add our own alias
+        # java
+        append_to_file("## PATH", zshrc_file)
+        append_to_file("# JAVA", zshrc_file)
+        # java_path = run("which java")
+        # text = "export JAVA_HOME=%s" % java_path.replace('/jre/bin/java', '')
+        # append_to_file(text, zshrc_file)
+        # append_to_file("export PATH=$JAVA_HOME/bin:$PATH", zshrc_file)
+        # append_to_file("export JAVA_OPTS=\"-Xms1024m -Xmx20000m\"", zshrc_file)
 
-        #Add our own alias
-        #java
-        f.write("## PATH")
-        f.write("# JAVA")
-        java_path = run("which java")
-        f.write("export JAVA_HOME=%s" % java_path.replace('/jre/bin/java',''))
-        f.write("export PATH=$JAVA_HOME/bin:$PATH")
-        f.write("export JAVA_OPTS=\"-Xms1024m -Xmx20000m\"")
 
-        #maven
-        f.write("# MAVEN")
-        f.write("export MAVEN_HOME=/opt/maven")
-        f.write("export PATH=$PATH:$MAVEN_HOME/bin")
-        f.write("export MAVEN_OPTS=\"-Xmx1024m\"")
 
-        #intellij
-        f.write("# INTELLIJ")
-        f.write("export INTELLIJ_HOME=%s/intellij" % settings['working_directory'])
-        f.write("export PATH=$PATH:$INTELLIJ_HOME/bin")
-
-        #tomcat
-        f.write("# TOMCAT")
-        f.write("export CATALINA_HOME=%s/tomcat")
-        f.write("export CATALINA_OPTS=\"$CATALINA_OPTS -Xms256m\"")
-
-        #liquibase
-        f.write("# LIQUIBASE")
-        f.write("export LIQUIBASE_HOME=/opt/liquibase")
-        f.write("export PATH=$PATH:$LIQUIBASE_HOME")
-
-        #ant
-        # f.write("# ANT")
-        # export ANT_HOME=/opt/ant
-        # export ANT_OPTS="-Dfile.encoding=utf-8"
-        # export PATH=$ANT_HOME/bin:$PATH
-
-        #proxy settings
-        f.write("# PROXY")
-        proxy_addr = "http://%s:%s@%s:%s" %(settings['proxy']['username'], settings['proxy']['pwd'], settings['proxy']['host'], settings['proxy']['port'])
-        f.write("no_proxy=localhost,127.0.0.1,172.16.0.0/12,10.0.0.0/8,*.site-mairie.noumea.nc,`/bin/hostname`")
-        f.write("http_proxy=%s" % proxy_addr)
-        f.write("https_proxy=%s" % proxy_addr)
-        f.write("ftp_proxy=%s" % proxy_addr)
-        f.write("export http_proxy")
-        f.write("export https_proxy")
-        f.write("export ftp_proxy")
-        f.write("export no_proxy")
-        f.write("export HTTP_PROXY=$http_proxy")
-        f.write("export HTTPS_PROXY=$https_proxy")
-        f.write("export FTP_PROXY=$ftp_proxy")
-
-        f.write("## CUSTOM ALIAS")
-        f.write("alias ll=\"ls -ltr\"")
-        f.write("alias squirrel=\"/opt/squirrel/squirrel-sql.sh\"")
-        f.write("alias fakeSMTP=\"sudo java -jar %/fakeSMTP/fakeSMTP-%s.jar -o . %/fakeSMTP/received-emails  -a 127.0.0.1\"")
-        f.write("alias px='ps auxf | grep -v grep | grep -i -e VSZ -e'")
-        f.write("alias df='pydf'")
-        f.write("alias hist='history | grep'")
-        f.write("#Apache Directory Studio")
-        f.write("alias ads=\"%s/apacheDirectoryStudio/ApacheDirectoryStudio\"" % settings['working_directory'])
-        f.write("#Datastudio")
-        f.write("alias datastudio=\"%s/DevTools/datastudio/datastudio.sh\"" % settings['working_directory'])
-        f.write("#Intellij")
-        f.write("alias intellij=\"$INTELLIJ_HOME/bin/idea.sh\"")
-
-        f.write("# Docker")
-        f.write("# Kill all running containers.")
-        f.write("alias dockerkillall='docker kill $(docker ps -q)'")
-        f.write("# Delete all stopped containers.")
-        f.write("alias dockercleanc='printf \"\n>>> Deleting stopped containers\n\n\" && docker rm $(docker ps -a -q)'")
-        f.write("# Delete all untagged images.")
-        f.write("alias dockercleani='printf \"\n>>> Deleting untagged images\n\n\" && docker rmi $(docker images -q -f dangling=true)'")
-        f.write("# Delete all stopped containers and untagged images.")
-        f.write("alias dockerclean='dockercleanc || true && dockercleani'")
-        f.write("# Get image ip")
-        f.write("alias dps=\"docker ps -q | xargs docker inspect --format '{{ .Id }} - {{ .Name }} - {{ .NetworkSettings.IPAddress }}'\"")
-        f.write("# BFG Repo-Cleaner")
-        f.write("alias bfg=\"java -jar %s/bfg-repo-cleaner/bfg.jar &&\"" % settings['working_directory'])
-
-        f.close()
+        # f.write("## PATH")
+        # f.write("# JAVA")
+        # java_path = run("which java")
+        # f.write("export JAVA_HOME=%s" % java_path.replace('/jre/bin/java',''))
+        # f.write("export PATH=$JAVA_HOME/bin:$PATH")
+        # f.write("export JAVA_OPTS=\"-Xms1024m -Xmx20000m\"")
+        #
+        # #maven
+        # f.write("# MAVEN")
+        # f.write("export MAVEN_HOME=/opt/maven")
+        # f.write("export PATH=$PATH:$MAVEN_HOME/bin")
+        # f.write("export MAVEN_OPTS=\"-Xmx1024m\"")
+        #
+        # #intellij
+        # f.write("# INTELLIJ")
+        # f.write("export INTELLIJ_HOME=%s/intellij" % settings['working_directory'])
+        # f.write("export PATH=$PATH:$INTELLIJ_HOME/bin")
+        #
+        # #tomcat
+        # f.write("# TOMCAT")
+        # f.write("export CATALINA_HOME=%s/tomcat")
+        # f.write("export CATALINA_OPTS=\"$CATALINA_OPTS -Xms256m\"")
+        #
+        # #liquibase
+        # f.write("# LIQUIBASE")
+        # f.write("export LIQUIBASE_HOME=/opt/liquibase")
+        # f.write("export PATH=$PATH:$LIQUIBASE_HOME")
+        #
+        # #ant
+        # # f.write("# ANT")
+        # # export ANT_HOME=/opt/ant
+        # # export ANT_OPTS="-Dfile.encoding=utf-8"
+        # # export PATH=$ANT_HOME/bin:$PATH
+        #
+        # #proxy settings
+        # f.write("# PROXY")
+        # proxy_addr = "http://%s:%s@%s:%s" %(settings['proxy']['username'], settings['proxy']['pwd'], settings['proxy']['host'], settings['proxy']['port'])
+        # f.write("no_proxy=localhost,127.0.0.1,172.16.0.0/12,10.0.0.0/8,*.site-mairie.noumea.nc,`/bin/hostname`")
+        # f.write("http_proxy=%s" % proxy_addr)
+        # f.write("https_proxy=%s" % proxy_addr)
+        # f.write("ftp_proxy=%s" % proxy_addr)
+        # f.write("export http_proxy")
+        # f.write("export https_proxy")
+        # f.write("export ftp_proxy")
+        # f.write("export no_proxy")
+        # f.write("export HTTP_PROXY=$http_proxy")
+        # f.write("export HTTPS_PROXY=$https_proxy")
+        # f.write("export FTP_PROXY=$ftp_proxy")
+        #
+        # f.write("## CUSTOM ALIAS")
+        # f.write("alias ll=\"ls -ltr\"")
+        # f.write("alias squirrel=\"/opt/squirrel/squirrel-sql.sh\"")
+        # f.write("alias fakeSMTP=\"sudo java -jar %/fakeSMTP/fakeSMTP-%s.jar -o . %/fakeSMTP/received-emails  -a 127.0.0.1\"")
+        # f.write("alias px='ps auxf | grep -v grep | grep -i -e VSZ -e'")
+        # f.write("alias df='pydf'")
+        # f.write("alias hist='history | grep'")
+        # f.write("#Apache Directory Studio")
+        # f.write("alias ads=\"%s/apacheDirectoryStudio/ApacheDirectoryStudio\"" % settings['working_directory'])
+        # f.write("#Datastudio")
+        # f.write("alias datastudio=\"%s/DevTools/datastudio/datastudio.sh\"" % settings['working_directory'])
+        # f.write("#Intellij")
+        # f.write("alias intellij=\"$INTELLIJ_HOME/bin/idea.sh\"")
+        #
+        # f.write("# Docker")
+        # f.write("# Kill all running containers.")
+        # f.write("alias dockerkillall='docker kill $(docker ps -q)'")
+        # f.write("# Delete all stopped containers.")
+        # f.write("alias dockercleanc='printf \"\n>>> Deleting stopped containers\n\n\" && docker rm $(docker ps -a -q)'")
+        # f.write("# Delete all untagged images.")
+        # f.write("alias dockercleani='printf \"\n>>> Deleting untagged images\n\n\" && docker rmi $(docker images -q -f dangling=true)'")
+        # f.write("# Delete all stopped containers and untagged images.")
+        # f.write("alias dockerclean='dockercleanc || true && dockercleani'")
+        # f.write("# Get image ip")
+        # f.write("alias dps=\"docker ps -q | xargs docker inspect --format '{{ .Id }} - {{ .Name }} - {{ .NetworkSettings.IPAddress }}'\"")
+        # f.write("# BFG Repo-Cleaner")
+        # f.write("alias bfg=\"java -jar %s/bfg-repo-cleaner/bfg.jar &&\"" % settings['working_directory'])
+        #
+        # f.close()
